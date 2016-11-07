d7a4be61f58f7b2171fbfef2dd35d926cf8d2dcb
Michael Bravo <email": null>
2016-11-07 09:09:05 -0500

    up button working

--- app/controllers/votes.rb
+++ app/controllers/votes.rb
@@ -1,6 +1,10 @@
 post '/votes/new' do
   if logged_in
-    # p params[:question_id]
-    new_vote = Vote.new(value: 1, user_id: current_user.user_id, votable_id: params[:question_id])
+    new_vote = Vote.new(value: 1, user_id: current_user.id, votable_id: params[:votable_id], votable_type: params[:votable_type])
+    # question = Question.find(params[:votable_id])
+    if new_vote.valid?
+      new_vote.save
+      redirect "/questions/#{params[:votable_id]}"
+    end
   end
 end
